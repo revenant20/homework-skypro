26bb7fa1683cbc5f13c677207ad8eb390c7ac6d7
sazonovfm <sazonovfm@gmail.com>
2022-06-19 17:30:55 +0300

    project for start

--- src/main/java/ru/skypro/homework/dto/pass/NewPassword.java
+++
@@ -1,9 +0,0 @@
-package ru.skypro.homework.dto.pass;
-
-import lombok.Data;
-
-@Data
-public class NewPassword {
-    private String newPassword;
-    private String currentPassword;
-}
--- src/main/java/ru/skypro/homework/dto/pass/ResetPassword.java
+++
@@ -1,8 +0,0 @@
-package ru.skypro.homework.dto.pass;
-
-import lombok.Data;
-
-@Data
-public class ResetPassword {
-    private String email;
-}
