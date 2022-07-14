b23ce5989ca52bd7c188b3f7e615b105ce5f389a
sazonovfm <sazonovfm@gmail.com>
2022-07-14 20:11:24 +0300

    подготовил проект

--- src/main/java/ru/skypro/homework/service/AuthService.java
+++ src/main/java/ru/skypro/homework/service/AuthService.java
@@ -3,10 +3,7 @@ package ru.skypro.homework.service;
 import ru.skypro.homework.dto.RegReq;
 import ru.skypro.homework.dto.Role;
 
-import java.util.Optional;
-
 public interface AuthService {
     boolean login(String userName, String password);
     boolean register(RegReq regReq, Role role);
-    Optional<String> changePassword(String userName, String password, String newPassword);
 }
--- src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
@@ -8,11 +8,7 @@ import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 import ru.skypro.homework.dto.RegReq;
 import ru.skypro.homework.dto.Role;
-import ru.skypro.homework.repository.entity.UserEn;
-import ru.skypro.homework.repository.UserRepository;
 import ru.skypro.homework.service.AuthService;
-
-import java.util.Optional;
 
 @Service
 public class AuthServiceImpl implements AuthService {
@@ -20,11 +16,9 @@ public class AuthServiceImpl implements AuthService {
     private final UserDetailsManager manager;
 
     private final PasswordEncoder encoder;
-    private final UserRepository userRepository;
 
-    public AuthServiceImpl(UserDetailsManager manager, UserRepository userRepository) {
+    public AuthServiceImpl(UserDetailsManager manager) {
         this.manager = manager;
-        this.userRepository = userRepository;
         this.encoder = new BCryptPasswordEncoder();
     }
 
@@ -40,37 +34,10 @@ public class AuthServiceImpl implements AuthService {
     }
 
     @Override
-    public Optional<String> changePassword(String userName, String password, String newPassword) {
-        if (!manager.userExists(userName)) {
-            return Optional.empty();
-        }
-        UserDetails userDetails = manager.loadUserByUsername(userName);
-        String encryptedPassword = userDetails.getPassword();
-        String encryptedPasswordWithoutEncryptionType = encryptedPassword.substring(8);
-        System.out.println("PASS" + password);
-        System.out.println("OLD PASS" + encryptedPasswordWithoutEncryptionType);
-        if (!encoder.matches(password, encryptedPasswordWithoutEncryptionType)) {
-            return Optional.empty();
-        }
-        manager.changePassword(password, newPassword);
-        UserEn byUsername = userRepository.findByUsername(userName);
-        byUsername.setPassword(newPassword);
-        UserEn userEn = userRepository.saveAndFlush(byUsername);
-        return Optional.ofNullable(userEn.getPassword());
-    }
-
-    @Override
     public boolean register(RegReq regReq, Role role) {
         if (manager.userExists(regReq.getUsername())) {
             return false;
         }
-        UserEn entity = new UserEn();
-        entity.setFirstName(regReq.getFirstName());
-        entity.setLastName(regReq.getLastName());
-        entity.setPhone(regReq.getPhone());
-        entity.setUsername(regReq.getUsername());
-        entity.setPassword(regReq.getPassword());
-        userRepository.saveAndFlush(entity);
         manager.createUser(
                 User.withDefaultPasswordEncoder()
                         .password(regReq.getPassword())
